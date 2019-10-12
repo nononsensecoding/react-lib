164f5889f6cd6fc2f0d6909f7d4cb2403bcc7db8
Terry <nononsensecoding@gmail.com>
2019-10-11 20:08:13 -0400

    add crude hover state

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Button = ({ children }) => <button>{children}</button>;
+class Button extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      isHovering: props.isDefaultHovering
+    };
+
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+  }
+
+  handleMouseEnter() {
+    this.setHoverState(true);
+  }
+
+  handleMouseLeave() {
+    this.setHoverState(false);
+  }
+
+  setHoverState(isHovering) {
+    this.setState({ isHovering });
+  }
+
+  render() {
+    const { children, ...attrs } = this.props;
+    const { isHovering } = this.state;
+
+    return (
+      <button
+        {...attrs}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
+        {`${children} ${isHovering ? " hovering" : ""}`}
+      </button>
+    );
+  }
+}
 export default Button;
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -6,14 +6,20 @@ export default {
   title: "Button"
 };
 
-export const text = () => (
+export const idle = () => (
   <Button onClick={action("clicked")}>Hello Button</Button>
 );
 
-export const emoji = () => (
+export const emojiChildren = () => (
   <Button onClick={action("clicked")}>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
   </Button>
 );
+
+export const disabled = () => <Button disabled>Hello Button</Button>;
+
+export const focused = () => <Button focused>Hello Button</Button>;
+
+export const hover = () => <Button isDefaultHovering>Hello Button</Button>;
